b3c34fda59831af8bc9b03597aeb98e86a990f19
Dumsani Simelane <ntsetseleloset049@gmail.com>
2026-06-17 07:27:18 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>NexusIQ</h1>
+
+      <h2>
+        Train. Track. Comply. Perform.
+      </h2>
+
+      <p>
+        Powered by Miguel and Father Hygiene Solution
+      </p>
+
+      <button>
+        Start Free Trial
+      </button>
+
+    </main>
+  );
+    }
